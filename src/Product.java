f55205dac89024b713a8dded7fd60afc69245ff5
Артур <kpro83665@gmail.com>
2025-05-21 15:12:22 +0300

    Update Product.java

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Product {
+    String name;
+    double price;
+
+    Product(String n, double p) {
+        name = n;
+        price = p;
+    }
+
+    void displayInfo() {
+        System.out.println("Название: " + name);
+        System.out.println("Цена: " + price);
+    }
+
+    double getPrice() {
+        return price;
+    }
 }
