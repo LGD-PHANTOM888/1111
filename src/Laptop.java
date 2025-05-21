1159cecf0e217ed91f94ceb9642118bea607d591
Артур <kpro83665@gmail.com>
2025-05-21 15:11:31 +0300

    Update Laptop.java

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+public class Laptop extends Product {
+    String processor;
 
-public class Laptop {
+    Laptop(String n, double p, String pr) {
+        super(n, p);
+        processor = pr;
+    }
+
+    void displayInfo() {
+        System.out.println("Ноутбук:");
+        System.out.println("Название: " + name);
+        System.out.println("Цена: " + price);
+        System.out.println("Процессор: " + processor);
+    }
 }
