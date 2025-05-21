41abdcff2eff8b55d4ea9e860d26a6d5b13cb935
Артур <kpro83665@gmail.com>
2025-05-21 15:11:57 +0300

    Update Phone.java

--- src/Phone.java
+++ src/Phone.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Phone extends Product {
+    String brand;
 
-public class Phone {
+    Phone(String n, double p, String b) {
+        super(n, p);
+        brand = b;
+    }
+
+    void displayInfo() {
+        System.out.println("Телефон:");
+        System.out.println("Название: " + name);
+        System.out.println("Цена: " + price);
+        System.out.println("Бренд: " + brand);
+    }
+
 }
