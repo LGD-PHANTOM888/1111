39ccdbcf383467b57b089171b2f323be3ec762df
Артур <kpro83665@gmail.com>
2025-05-21 15:10:24 +0300

    Update Cart.java

--- src/Cart.java
+++ src/Cart.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
-
 public class Cart {
+    Product[] items = new Product[10];
+    int size = 0;
+
+    void add(Product p) {
+        if (size < items.length) {
+            items[size] = p;
+            size++;
+        } else {
+            System.out.println("Ошибка");
+        }
+    }
+
+    void displayCart() {
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < size; i++) {
+            System.out.println("Товар #" + (i + 1));
+            items[i].displayInfo();
+            System.out.println();
+        }
+    }
+
+    double getTotal() {
+        double total = 0;
+        for (int i = 0; i < size; i++) {
+            total += items[i].getPrice();
+        }
+        return total;
+    }
+
 }
